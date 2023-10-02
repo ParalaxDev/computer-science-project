b86479576ca3c643d2834ff126a43d2b7f4b501c
Will Baker <willmdbaker@gmail.com>
2023-10-02 22:21:51 +0100

    finished gate graph

--- x32-controller/ui/widgets/gate.py
+++ x32-controller/ui/widgets/gate.py
@@ -2,6 +2,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets, uic
 from PyQt6.QtCore import Qt
 from PySide6.QtCore import QRect
 import osc, core, utils
+import math
 
 class GateGraph(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
@@ -30,24 +31,36 @@ class GateGraph(QtWidgets.QWidget):
         painter.fillRect(rect, brush)
 
         thresh = self.parent().parent().parent().parent().SOURCE.GATE_THRESH
+        range = self.parent().parent().parent().parent().SOURCE.GATE_RANGE
+        active = self.parent().parent().parent().parent().SOURCE.GATE_ON
         thresh = utils.FloatToFader(thresh, 0, 1, 0, -80)
+        range = utils.FloatToFader(range, 0, 1, 60, 0)
 
         pen = QtGui.QPen()
         pen.setWidth(4)
-        pen.setColor(QtGui.QColor('white'))
+        pen.setColor(QtGui.QColor('blue') if active else QtGui.QColor('white'))
 
         painter.setPen(pen)
 
         w = painter.device().width()
         h = painter.device().height()
 
-        line1 = QtCore.QLineF(0 + (thresh * w), h - (thresh * h), w, 0)
-        line2 = QtCore.QLineF(0 + (thresh * w), h - (thresh * h), 0 + (thresh * w), h)
+        print(min(range, thresh))
 
-        
+        line1 = QtCore.QLineF()
+        line1.setP1(QtCore.QPointF((thresh * w), h - (thresh * h)))
+        line1.setAngle(45)
+        line1.setLength(1000)
+        line2 = QtCore.QLineF()
+        line2.setP1(QtCore.QPointF((thresh * w), h - (thresh * h)))
+        line2.setAngle(-90)
+        line2.setLength(range * .75 * h)
+        line3 = QtCore.QLineF()
+        line3.setP1(line2.p2())
+        line3.setAngle(225)
+        line3.setLength(1000)
 
-
-        painter.drawLines([line1, line2])
+        painter.drawLines([line1, line2, line3])
 
         painter.drawLines
 
