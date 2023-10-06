2e5be0e421eccee4138c019388820b38dd7e050e
Will Baker <willmdbaker@gmail.com>
2023-10-06 09:50:50 +0100

    basic db setup

--- x32-controller/db/controller.py
+++ x32-controller/db/controller.py
@@ -9,17 +9,18 @@ class Controller:
             os.makedirs('x32-controller/assets/db')
             utils.log.warn('database directory does not exist, its been created in /assets/db')
 
-        conn = sqlite3.connect('x32-controller/assets/db/main.db')
-        cursor = conn.cursor()
+        self.conn = sqlite3.connect('x32-controller/assets/db/main.db')
+        self.cursor = self.conn.cursor()
 
         try:
-            cursor.execute('DROP TABLE users')
-            cursor.execute('DROP TABLE base')
-            cursor.execute('DROP TABLE sessions')
+            self.cursor.execute('DROP TABLE users')
+            self.cursor.execute('DROP TABLE base')
+            self.cursor.execute('DROP TABLE saves')
+            self.cursor.execute('DROP TABLE channel')
         except:
             pass
 
-        cursor.execute('''
+        self.cursor.execute('''
             CREATE TABLE "base" (
                 "id" INTEGER UNIQUE,
                 "name" TEXT NOT NULL,
@@ -28,31 +29,75 @@ class Controller:
                 "link" INTEGER,
                 "delay_on" INTEGER,
                 "delay_time" REAL,
+                "gate_on" INTEGER,
+                "gate_thresh" REAL,
+                "gate_range" REAL,
+                "dyn_on" INTEGER,
+                "dyn_thresh" REAL,
+                "dyn_ratio" REAL,
+                "eq_on" INTEGER,
+                "eq_1_type" INTEGER,
+                "eq_1_f" REAL,
+                "eq_1_g" REAL,
+                "eq_1_q" REAL,
+                "eq_2_type" INTEGER,
+                "eq_2_f" REAL,
+                "eq_2_g" REAL,
+                "eq_2_q" REAL,
+                "eq_3_type" INTEGER,
+                "eq_3_f" REAL,
+                "eq_3_g" REAL,
+                "eq_3_q" REAL,
+                "eq_4_type" INTEGER,
+                "eq_4_f" REAL,
+                "eq_4_g" REAL,
+                "eq_4_q" REAL,
                 "gain" REAL,
                 "mute" INTEGER,
+                "pan" REAL,
                 PRIMARY KEY("id" AUTOINCREMENT)
             )
         ''')
 
-        cursor.execute('''
-            CREATE TABLE "sessions" (
+        self.cursor.execute('''
+            CREATE TABLE "channel" (
+                "id" INTEGER UNIQUE,
+                "base_id" INTEGER,
+                "headamp_source" INTEGER,
+                "headamp_gain" REAL,
+                "hp_on" INTEGER,
+                "hp_freq" REAL,
+                "phantom" INTEGER,
+                PRIMARY KEY("id" AUTOINCREMENT)
+                FOREIGN KEY ("base_id")
+                    REFERENCES "base" ("id")
+            )
+        ''')
+
+        self.cursor.execute('''
+            CREATE TABLE "saves" (
                 "id" INTEGER UNIQUE,
                 "name" TEXT NOT NULL,
                 "created_at" TEXT,
                 "updated_at" TEXT,
-                
                 PRIMARY KEY("id" AUTOINCREMENT)
             )
         ''')
         
-        cursor.execute('''
+        self.cursor.execute('''
             CREATE TABLE "users" (
                 "id" INTEGER UNIQUE,
                 "name" TEXT NOT NULL,
                 "password" TEXT,
-                "sessions_id" INTEGER,
+                "saves_id" INTEGER,
                 PRIMARY KEY ("id" AUTOINCREMENT),
-                FOREIGN KEY ("sessions_id")
-                    REFERENCES "sessions" ("id")
+                FOREIGN KEY ("saves_id")
+                    REFERENCES "saves" ("id")
             )
         ''')
+
+    def execute(self, sql):
+        try:
+            self.cursor.execute(sql)
+        except:
+            utils.log.error('Failed to execute SQL')
